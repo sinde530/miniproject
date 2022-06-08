eff88aeff2f5344df728fa76d22fd42a58093be7
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-08 20:22:10 +0900

    yarn add --dev @storybook/addon-storyshots react-test-renderer

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,17 @@
-import type { Config } from "@jest/types";
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
 
-// Sync object
-const config: Config.InitialOptions = {
-  verbose: true,
-};
-export default config;
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+import { matchers, createSerializer } from "@emotion/jest";
 
-// Or async function
-export default async (): Promise<Config.InitialOptions> => {
-  return {
-    verbose: true,
-  };
-};
+// https://github.com/vercel/next.js/issues/26749
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: () => "Next image stub", // whatever
+}));
+
+expect.extend(matchers);
+expect.addSnapshotSerializer(createSerializer());
---
+++ src/App.test.ts
@@ -0,0 +1,10 @@
+import { render } from "react-dom"
+
+import App from './App';
+
+
+describe('App', () => {     
+    const {container} =  render(<App/>)
+})
+
+export {}
