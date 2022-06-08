1e87689559f15cdb105362b420443bbc2dc6867f
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-08 18:47:45 +0900

    store stop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Greetings from "./components/Greetings";
 
 import "./App.css";
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   return (
     <div className="App">
+      <CounterContainer />
       <Greetings name="Crong" mark="!" />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
 
-import store from "./app/store";
+import { createStore } from "redux";
+
+import rootReducer from "./modules";
 
 import App from "./App";
 
 import "./index.css";
+
+const store = createStore(rootReducer);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
