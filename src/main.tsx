aa7f6ca6da513750691d158c3a2d9afccc6ec7d6
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-08 17:41:02 +0900

    eslint install

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 
-import { createStore } from 'redux'
+import { createStore } from "redux";
 
-import rootReducer from './modules'
+import rootReducer from "./modules";
 
-import App from './App'
+import App from "./App";
 
-import './index.css'
+import "./index.css";
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store} >
-      <App />
-    </Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
