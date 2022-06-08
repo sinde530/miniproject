e28e04e69a0344e9849a58cf6c29ea7997aef97a
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-08 17:50:29 +0900

    store component folder create

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
 
-import { createStore } from "redux";
-
-import rootReducer from "./modules";
+import store from "./app/store";
 
 import App from "./App";
 
 import "./index.css";
-
-const store = createStore(rootReducer);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
