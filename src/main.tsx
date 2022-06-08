440358c5936bac10a479e4d35c6455d0a8760d4f
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-08 17:39:43 +0900

    main modify

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { Provider } from 'react-redux'
+
+import { createStore } from 'redux'
+
+import rootReducer from './modules'
+
 import App from './App'
+
 import './index.css'
 
+const store = createStore(rootReducer)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store} >
+      <App />
+    </Provider>
 )
